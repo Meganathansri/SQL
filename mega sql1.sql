a4f2c973f1fc0792ed93530ab2dca4cf915d21ea
Meganathansri <91050907+Meganathansri@users.noreply.github.com>
2022-11-07 21:07:20 +0530

    Add files via upload

---
+++ mega sql1.sql
@@ -0,0 +1,8 @@
+/*
+create database meganathan;
+use meganathan;
+create table students( regno int, sname varchar(30),gender char (10), mobilenumber int, eng int, maths int, science int, social int);
+select * from students;
+insert into students values(1,'meg', 'male',1234569878, 55,85,99,55);
+insert into students value (2, 'tam', 'female',56456654, 88,98,78,99);
+alter table students add total in
